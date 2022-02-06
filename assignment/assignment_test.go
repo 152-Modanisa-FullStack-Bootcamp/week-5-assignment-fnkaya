faeae34534c96894fe40771633b524c99eda0a75
Furkan Kaya <furkan.kaya2@obss.com.tr>
2022-02-06 23:38:05 +0300

    assignment 5

--- assignment/assignment_test.go
+++ assignment/assignment_test.go
@@ -8,111 +8,133 @@ import (
 )
 
 func TestAddUint32(t *testing.T) {
-	/*
-		Sum uint32 numbers, return uint32 sum value and boolean overflow flag
-		cases need to pass:
-			math.MaxUint32, 1 => 0, true
-			1, 1 => 2, false
-			42, 2701 => 2743, false
-			42, math.MaxUint32 => 41, true
-			4294967290, 5 => 4294967295, false
-			4294967290, 6 => 0, true
-			4294967290, 10 => 4, true
-	*/
-	sum, overflow := AddUint32(math.MaxUint32, 1)
+	tcs := []struct {
+		given            [2]uint32
+		expectedSum      uint32
+		expectedOverflow bool
+	}{
+		{[2]uint32{math.MaxUint32, 1}, 0, true},
+		{[2]uint32{1, 1}, 2, false},
+		{[2]uint32{42, 2701}, 2743, false},
+		{[2]uint32{42, math.MaxUint32}, 41, true},
+		{[2]uint32{4294967290, 5}, 4294967295, false},
+		{[2]uint32{4294967290, 6}, 0, true},
+		{[2]uint32{4294967290, 10}, 4, true},
+	}
 
-	assert.Equal(t, uint32(0), sum)
-	assert.True(t, overflow)
+	for _, tc := range tcs {
+		sum, overflow := AddUint32(tc.given[0], tc.given[1])
+		assert.Equal(t, tc.expectedSum, sum)
+		assert.Equal(t, tc.expectedOverflow, overflow)
+	}
 }
 
 func TestCeilNumber(t *testing.T) {
-	/*
-		Ceil the number within 0.25
-		cases need to pass:
-			42.42 => 42.50
-			42 => 42
-			42.01 => 42.25
-			42.24 => 42.25
-			42.25 => 42.25
-			42.26 => 42.50
-			42.55 => 42.75
-			42.75 => 42.75
-			42.76 => 43
-			42.99 => 43
-			43.13 => 43.25
-	*/
-	point := CeilNumber(42.42)
+	tcs := []struct {
+		given    float64
+		expected float64
+	}{
+		{42.42, 42.50},
+		{42, 42},
+		{42.01, 42.25},
+		{42.24, 42.25},
+		{42.25, 42.25},
+		{42.26, 42.50},
+		{42.55, 42.75},
+		{42.75, 42.75},
+		{42.76, 43},
+		{42.99, 43},
+		{43.13, 43.25},
+	}
 
-	assert.Equal(t, 42.50, point)
+	for _, tc := range tcs {
+		result := CeilNumber(tc.given)
+		assert.Equal(t, tc.expected, result)
+	}
 }
 
 func TestAlphabetSoup(t *testing.T) {
-	/*
-		String with the letters in alphabetical order.
-		cases need to pass:
-		 	"hello" => "ehllo"
-			"" => ""
-			"h" => "h"
-			"ab" => "ab"
-			"ba" => "ab"
-			"bac" => "abc"
-			"cba" => "abc"
-	*/
-	result := AlphabetSoup("hello")
+	tcs := []struct {
+		given    string
+		expected string
+	}{
+		{"hello", "ehllo"},
+		{"", ""},
+		{"h", "h"},
+		{"ab", "ab"},
+		{"ba", "ab"},
+		{"bac", "abc"},
+		{"cba", "abc"},
+	}
 
-	assert.Equal(t, "ehllo", result)
+	for _, tc := range tcs {
+		output := AlphabetSoup(tc.given)
+		assert.Equal(t, tc.expected, output)
+	}
 }
 
 func TestStringMask(t *testing.T) {
-	/*
-		Replace after n(uint) character of string with '*' character.
-		cases need to pass:
-			"!mysecret*", 2 => "!m********"
-			"", n(any positive number) => "*"
-			"a", 1 => "*"
-			"string", 0 => "******"
-			"string", 3 => "str***"
-			"string", 5 => "strin*"
-			"string", 6 => "******"
-			"string", 7(bigger than len of "string") => "******"
-			"s*r*n*", 3 => "s*r***"
-	*/
-	result := StringMask("!mysecret*", 2)
+	tcs := []struct {
+		givenValue   string
+		givenUnmaskC uint
+		expected     string
+	}{
+		{"!mysecret*", 2, "!m********"},
+		{"", 100, "*"},
+		{"a", 1, "*"},
+		{"string", 0, "******"},
+		{"string", 3, "str***"},
+		{"string", 5, "strin*"},
+		{"string", 6, "******"},
+		{"string", 7, "******"},
+		{"s*r*n*", 3, "s*r***"},
+	}
 
-	assert.Equal(t, "!m********", result)
+	for _, tc := range tcs {
+		output := StringMask(tc.givenValue, tc.givenUnmaskC)
+		assert.Equal(t, tc.expected, output)
+	}
 }
 
 func TestWordSplit(t *testing.T) {
 	words := "apple,bat,cat,goodbye,hello,yellow,why"
-	/*
-		Your goal is to determine if the first element in the array can be split into two words,
-		where both words exist in the dictionary(words variable) that is provided in the second element of array.
+	tcs := []struct {
+		given    [2]string
+		expected interface{}
+		// I prefer to change expected value as interface
+	}{
+		{[2]string{"hellocat", words}, [2]string{"hello", "cat"}},
+		{[2]string{"catbat", words}, [2]string{"cat", "bat"}},
+		{[2]string{"yellowapple", words}, [2]string{"yellow", "apple"}},
+		{[2]string{"", words}, "not possible"},
+		{[2]string{"notcat", words}, "not possible"},
+		{[2]string{"bootcamprocks!", words}, "not possible"},
+	}
 
-		cases need to pass:
-			[2]string{"hellocat",words} => hello,cat
-			[2]string{"catbat",words} => cat,bat
-			[2]string{"yellowapple",words} => yellow,apple
-			[2]string{"",words} => not possible
-			[2]string{"notcat",words} => not possible
-			[2]string{"bootcamprocks!",words} => not possible
-	*/
-	result := WordSplit([2]string{"hellocat", words})
-
-	assert.Equal(t, "hello,cat", result)
+	for _, tc := range tcs {
+		output := WordSplit(tc.given)
+		assert.ObjectsAreEqualValues(tc.expected, output)
+	}
 }
 
 func TestVariadicSet(t *testing.T) {
-	/*
-		FINAL BOSS ALERT :)
-		Tip: Learn and apply golang variadic functions(search engine -> "golang variadic function" -> WOW You can really dance! )
+	tcs := []struct {
+		given    []interface{}
+		expected []interface{}
+	}{
+		{
+			[]interface{}{4, 2, 5, 4, 2, 4},
+			[]interface{}{4, 2, 5}},
+		{
+			[]interface{}{"bootcamp", "rocks!", "really", "rocks!"},
+			[]interface{}{"bootcamp", "rocks!", "really"}},
+		{
+			[]interface{}{1, uint32(1), "first", 2, uint32(2), "second", 1, uint32(2), "first"},
+			[]interface{}{1, uint32(1), "first", 2, uint32(2), "second"}},
+	}
 
-		Convert inputs to set(no duplicate element)
-		cases need to pass:
-			4,2,5,4,2,4 => []interface{4,2,5}
-			"bootcamp","rocks!","really","rocks! => []interface{"bootcamp","rocks!","really"}
-			1,uint32(1),"first",2,uint32(2),"second",1,uint32(2),"first" => []interface{1,uint32(1),"first",2,uint32(2),"second"}
-	*/
-	set := VariadicSet(4, 2, 5, 4, 2, 4)
-
-	assert.Equal(t, []interface{}{4, 2, 5}, set)
+	for _, tc := range tcs {
+		output := VariadicSet(tc.given...)
+		assert.ElementsMatch(t, tc.expected, output)
+	}
 }
